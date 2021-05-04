32dae356bcd5fd352906ff73a142bf447f26681b
Joseph Knopke <josephknopke@gmail.com>
2021-05-04 01:59:49 -0500

    logging in and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  root "sessions#home"
+
   get '/signup' => 'users#new'
   post '/signup' => 'users#create'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+
+  delete '/logout' => 'sessions#destroy'
   
   resources :comments
   #resources :genres
