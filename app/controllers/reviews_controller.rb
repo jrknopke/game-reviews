cf347c8ec21bd3a77a9d1201590c0c9865225058
Joseph Knopke <josephknopke@gmail.com>
2021-05-04 15:02:19 -0500

    review controller and form

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,20 @@
 class ReviewsController < ApplicationController
+    def new
+        @review = Review.new
+    end
+
+    def create
+        @review = current_user.reviews.build(review_params)
+        if @review.save
+            redirect_to reviews_path
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def review_params
+        params.require(:review).permit(:title, :content)
+    end
 end
