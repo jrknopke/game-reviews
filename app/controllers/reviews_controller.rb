61e9c66cb5367e9d8a35e08d0ea6cef59d566e74
Joseph Knopke <josephknopke@gmail.com>
2021-05-07 14:00:47 -0500

    reviews index display fix

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -18,6 +18,20 @@ class ReviewsController < ApplicationController
         end
     end
 
+    def index
+        if params[:user_id] && @user = User.find_by_id(params[:user_id])
+            @reviews = @user.reviews
+        else
+            @error = "That user doesn't exist" if params[:user_id]
+            @reviews = Review.all
+        end
+    end
+
+    def show
+        @review = Review.find_by_id(params[:id])
+        redirect_to reviews_path if !@review
+    end
+
     private
 
     def review_params
