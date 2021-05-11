4814a4f6dec4d5901fbf5ed0ccc0b303b320b22a
Joseph Knopke <josephknopke@gmail.com>
2021-05-11 13:05:54 -0500

    index changes

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,2 +1,9 @@
 module ReviewsHelper
+    def display_index_helper
+        if @user
+            content_tag(:h1, "#{@user.username}'s Posts")
+        else
+            content_tag(:h1, "All Posts")
+        end
+    end
 end
